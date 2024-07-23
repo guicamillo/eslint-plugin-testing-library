20fdad3c494a679566f91fe0ca194e0c94fd476d
Guilherme <guicamillo@gmail.com>
2024-07-23 14:42:26 -0700

    feat(prefer-present-queries): adding an auto-fixer for this rule

--- lib/rules/prefer-presence-queries.ts
+++ lib/rules/prefer-presence-queries.ts
@@ -1,4 +1,9 @@
-import { TSESTree } from '@typescript-eslint/utils';
+import {
+	AST_NODE_TYPES,
+	ASTUtils,
+	TSESLint,
+	TSESTree,
+} from '@typescript-eslint/utils';
 
 import { createTestingLibraryRule } from '../create-testing-library-rule';
 import { findClosestCallNode, isMemberExpression } from '../node-utils';
@@ -12,9 +17,98 @@ export type Options = [
 	}
 ];
 
+function findRenderDefinitionDeclaration(
+	scope: TSESLint.Scope.Scope,
+	query: string
+): TSESTree.Identifier | null {
+	if (!scope) {
+		return null;
+	}
+
+	const variable = scope.variables.find(
+		(v: TSESLint.Scope.Variable) => v.name === query
+	);
+
+	if (variable) {
+		return (
+			variable.defs
+				.map(({ name }) => name)
+				.filter(ASTUtils.isIdentifier)
+				.find(({ name }) => name === query) ?? null
+		);
+	}
+	if (!scope.upper) return null;
+
+	return findRenderDefinitionDeclaration(scope.upper, query);
+}
+
+function findOriginalMethodDeclaration(
+	context: TSESLint.Scope.Scope,
+	originalMethodName: string,
+	newMethodName: string
+) {
+	const originalMethodLocation = findRenderDefinitionDeclaration(
+		context,
+		originalMethodName
+	);
+
+	if (!originalMethodLocation) return;
+
+	const definition = originalMethodLocation.parent?.parent;
+	if (definition?.type !== AST_NODE_TYPES.ObjectPattern) return;
+
+	const properties = definition.properties;
+	const hasNewMethod = properties
+		.filter((p) => p.type === AST_NODE_TYPES.Property)
+		.some((p) => {
+			return (
+				p.kind === 'init' &&
+				//eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+				p.key?.type === AST_NODE_TYPES.Identifier &&
+				//eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+				p.key?.name === newMethodName
+			);
+		});
+
+	if (!hasNewMethod) {
+		return originalMethodLocation.range;
+	}
+}
+
+function fixFactory(
+	node: TSESTree.Node,
+	type: MessageIds,
+	scope: TSESLint.Scope.Scope
+) {
+	const m = {
+		wrongAbsenceQuery: ['getBy', 'queryBy'],
+		wrongPresenceQuery: ['queryBy', 'getBy'],
+	};
+
+	return function (fixer: TSESLint.RuleFixer) {
+		if (node.type !== AST_NODE_TYPES.Identifier) {
+			return [];
+		}
+
+		const [from, to] = m[type];
+
+		const currentName = node.name;
+		const targetName = currentName.replace(from, to);
+
+		const nodeFixer = fixer.replaceText(node, targetName);
+		const range = findOriginalMethodDeclaration(scope, currentName, targetName);
+		if (node.name.startsWith('screen') || !range) {
+			return nodeFixer;
+		}
+
+		return [{ range, text: [currentName, targetName].join(', ') }, nodeFixer];
+	};
+}
+
 export default createTestingLibraryRule<Options, MessageIds>({
 	name: RULE_NAME,
 	meta: {
+		fixable: 'code',
 		docs: {
 			description:
 				'Ensure appropriate `get*`/`query*` queries are used with their respective matchers',
@@ -85,14 +179,22 @@ export default createTestingLibraryRule<Options, MessageIds>({
 					(withinCallNode || isPresenceAssert) &&
 					!isPresenceQuery
 				) {
-					context.report({ node, messageId: 'wrongPresenceQuery' });
+					context.report({
+						node,
+						messageId: 'wrongPresenceQuery',
+						fix: fixFactory(node, 'wrongPresenceQuery', context.getScope()),
+					});
 				} else if (
 					!withinCallNode &&
 					absence &&
 					isAbsenceAssert &&
 					isPresenceQuery
 				) {
-					context.report({ node, messageId: 'wrongAbsenceQuery' });
+					context.report({
+						node,
+						messageId: 'wrongAbsenceQuery',
+						fix: fixFactory(node, 'wrongAbsenceQuery', context.getScope()),
+					});
 				}
 			},
 		};
